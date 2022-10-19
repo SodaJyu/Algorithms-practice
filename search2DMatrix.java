bcd74f736a740ae85083e9b55369e19c948d9cfc
Dominic Lowes <chaiskeng@gmail.com>
2022-10-19 10:37:50 +0900

    feat: add solution to searchMatrix

---
+++ search2DMatrix.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) return false;
+        
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+        
+        int left = 0, right = rows * columns - 1, mid;
+        
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            int midpoint_element = matrix[mid / columns][mid % columns];
+            if (midpoint_element == target) {
+                return true;
+            } else {
+                if (target < midpoint_element) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return false;
+    }
+}
