3e99132ae827d3d16aad5383af16e661500fde97
Dominic Lowes <chaiskeng@gmail.com>
2022-11-23 15:12:43 +0900

    feat: add cpp solution for n-ary tree max depth

--- maxDepthOfBinaryTree.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root is None: return 0
-        maxLeft = self.maxDepth(root.left)
-        maxRight = self.maxDepth(root.right)
-        return max(maxRight, maxLeft) + 1
