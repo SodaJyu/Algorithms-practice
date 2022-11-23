510a1b8f29bab503daa9ef8b83cc1f6d85f42691
Dominic Lowes <chaiskeng@gmail.com>
2022-11-23 15:09:23 +0900

    refactor: create Java directory

---
+++ java/howManyApples.java
@@ -0,0 +1,14 @@
+package java;
+class Solution {
+    public int maxNumberOfApples(int[] weight) {
+        int maxLoad = 5000;
+        int count = 0;
+        Arrays.sort(weight);
+        for (int i : weight) {
+            if (i > maxLoad) continue;
+            maxLoad -= i;
+            count++;
+        }
+        return count;
+    }
+}
---
+++ java/palindromeLinkedList.java
@@ -0,0 +1,37 @@
+package java;
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        // send a slow pointer to middle, fast pointer to the end. reverse the second half of the list and then compare first and second halves.
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        slow = reverse(slow);
+        fast = head;
+        
+        while(slow != null) {
+            if (slow.val != fast.val) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return true;
+        
+        
+    }
+    public ListNode reverse(ListNode root) {
+        ListNode prev = null;
+        while (root != null) {
+            ListNode next = root.next;
+            root.next = prev;
+            prev = root;
+            root = next;
+        }
+        return prev;
+    }
+}
