80e220ad142d7e9a3bbadc0a87d414aa5e239645
Dominic Lowes <chaiskeng@gmail.com>
2022-11-30 15:04:14 +0900

    feat: add solution for max area of islands

---
+++ LeetCode/cpp/medium/695-maxAreaOfIslands.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxi = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    maxi = max(maxi, helper(grid, i, j));
+                }
+            }
+        }
+        return maxi;
+    }
+    
+    int helper(vector<vector<int>>& grid, int i, int j) {
+        if (i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] == 0) {
+            return 0;
+        }
+        grid[i][j] = 0;
+        return (1 + helper(grid, i + 1, j) + helper(grid, i - 1, j) + helper(grid, i, j + 1) + helper(grid, i, j - 1));
+
+    }
+};
